cc71cad7c3086d5d1910cdbd08f34c5047b04ebb
Tefo - <Tefo ->
2015-11-24 22:44:35 -0300

    Add base project

--- myapp/routes/artists.js
+++ myapp/routes/artists.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var Artist = require('../')
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	var artists = [
 		"Justin",
 		"Madonna",
 		"Shakira"
-	]
+	];
   res.render('artists/index', { title: 'Artists', artists: artists });
 });
 
